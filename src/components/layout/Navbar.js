53838438ffd9c7005fd06d7716d80b5beea1cb1f
devzonedz <devzonegithub@gmail.com>
2021-08-06 03:28:09 +0100

    add sidenav

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,32 @@
 import React from 'react';
-import { Box, Heading, Flex, Text, useColorMode, Image } from '@chakra-ui/core';
+import {
+  Drawer,
+  DrawerBody,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  Box,
+  Heading,
+  Flex,
+  Badge,
+  Text,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  useToast,
+  useDisclosure,
+  Image,
+  useColorMode,
+  Input,
+  Button,
+
+  Stack,
+  useColorModeValue,
+
+} from '@chakra-ui/core';
 // import { ChevronDownIcon } from '@chakra-ui/icons';
 import { Link } from 'react-router-dom';
 import { FaMoon, FaSun } from 'react-icons/fa';
@@ -12,130 +39,136 @@ export default function Navbar(props) {
   const bg = { light: '#000000', dark: '#1a202c' };
   const color = { light: 'white', dark: 'white' };
 
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   const [show, setShow] = React.useState(false);
   const handleToggle = () => setShow(!show);
+  const btnRef = React.useRef()
+
 
   return (
-    <Flex
-      style={{ position: 'fixed', width: '100%', zIndex: '99', bottom: 0 }}
-      as="nav"
-      align="center"
-      justify="space-between"
-      wrap="wrap"
-      padding="0.5rem"
-      borderTop="1px solid white"
-      color={color[colorMode]}
-      bg={bg[colorMode]}
-    >
-      <Flex align="center" mr={5}>
-        <Link to="/">
-          <Heading
-            cursor="pointer"
-            fontWeight="bold"
-            as="h1"
-            fontSize="36px"
-            letterSpacing={'-.1rem'}
+      <>
+        <Flex
+            style={{ position: 'fixed', width: '100%', zIndex: '99', bottom: 0 }}
+            as="nav"
+            align="center"
+            justify="space-between"
+            wrap="wrap"
+            padding="0.5rem"
+            borderTop="1px solid white"
+            color={color[colorMode]}
+            bg={bg[colorMode]}
+        >
+          <Flex align="center" mr={5}>
+            <Link to="/">
+              <Heading
+                  cursor="pointer"
+                  fontWeight="bold"
+                  as="h1"
+                  fontSize="36px"
+                  letterSpacing={'-.1rem'}
+              >
+                <Flex>
+                  <Image
+                      loading="lazy"
+                      w="250px"
+                      className="main-logo"
+                      src={logo}
+                  ></Image>
+                  {/* <Image w="30px" className="main-logo" src="/images/center-logo.png"></Image> */}
+                </Flex>
+              </Heading>
+            </Link>
+          </Flex>
+
+          <Box display={{ base: 'block', md: 'none' }} onClick={onOpen}>
+            <GiHamburgerMenu
+                style={{ marginLeft: 10, fontSize: 28 }}
+            ></GiHamburgerMenu>
+          </Box>
+
+          <Box
+              display={{ base: show ? 'block' : 'none', md: 'flex' }}
+              width={{ base: 'full', md: 'auto' }}
+              alignItems="center"
+              textAlign="center"
+              justifyContent="flex-end"
+              flexGrow={1}
           >
-            <Flex>
-              <Image
-                loading="lazy"
-                w="250px"
-                className="main-logo"
-                src={logo}
-              ></Image>
-              {/* <Image w="30px" className="main-logo" src="/images/center-logo.png"></Image> */}
-            </Flex>
-          </Heading>
-        </Link>
-      </Flex>
-
-      <Box display={{ base: 'block', md: 'none' }} onClick={handleToggle}>
-        <GiHamburgerMenu
-          style={{ marginLeft: 10, fontSize: 28 }}
-        ></GiHamburgerMenu>
-      </Box>
-
-      <Box
-        display={{ base: show ? 'block' : 'none', md: 'flex' }}
-        width={{ base: 'full', md: 'auto' }}
-        alignItems="center"
-        textAlign="center"
-        justifyContent="flex-end"
-        flexGrow={1}
-      >
-        <Text
-          fontWeight="bold"
-          mt={{ base: 4, md: 0 }}
-          ml={8}
-          display="block"
-          fontSize="xl"
-          fontFamily="diodrum-bold !important"
-        >
-          <Link onClick={handleToggle} to="/about">
-            عن المركز
-          </Link>
-        </Text>
-        <Text
-          fontWeight="bold"
-          mt={{ base: 4, md: 0 }}
-          ml={8}
-          display="block"
-          fontSize="xl"
-          fontFamily="diodrum-bold !important"
-        >
-          <Link onClick={handleToggle} to="/enthropology">
-            المختار الإنثربولوجي
-          </Link>
-        </Text>
-        <Text
-          fontWeight="bold"
-          mt={{ base: 4, md: 0 }}
-          ml={8}
-          display="block"
-          fontSize="xl"
-          fontFamily="diodrum-bold !important"
-        >
-          <Link onClick={handleToggle} to="/cultureStudies">
-            دراسات ثقافية
-          </Link>
-        </Text>
-        <Text
-          fontWeight="bold"
-          mt={{ base: 4, md: 0 }}
-          ml={8}
-          display="block"
-          fontSize="xl"
-          fontFamily="diodrum-bold !important"
-        >
-          <Link onClick={handleToggle} to="/books?category=دراسات المركز">
-            كتب المركز
-          </Link>
-        </Text>
-        <Text
-          fontWeight="bold"
-          mt={{ base: 4, md: 0 }}
-          ml={8}
-          display="block"
-          fontSize="xl"
-          fontFamily="diodrum-bold !important"
-        >
-          <Link onClick={handleToggle} to="/events">
-            نشاطات
-          </Link>
-        </Text>
-        <Text
-          fontWeight="bold"
-          mt={{ base: 4, md: 0 }}
-          ml={8}
-          display="block"
-          fontSize="xl"
-          fontFamily="diodrum-bold !important"
-        >
-          <Link onClick={handleToggle} to="/standards">
-            المنهجية
-          </Link>
-        </Text>
-        {/* <Text             fontWeight="bold"
+            <Text
+                fontWeight="bold"
+                mt={{ base: 4, md: 0 }}
+                ml={8}
+                display="block"
+                fontSize="xl"
+                fontFamily="diodrum-bold !important"
+            >
+              <Link onClick={handleToggle} to="/about">
+                عن المركز
+              </Link>
+            </Text>
+            <Text
+                fontWeight="bold"
+                mt={{ base: 4, md: 0 }}
+                ml={8}
+                display="block"
+                fontSize="xl"
+                fontFamily="diodrum-bold !important"
+            >
+              <Link onClick={handleToggle} to="/enthropology">
+                المختار الإنثربولوجي
+              </Link>
+            </Text>
+            <Text
+                fontWeight="bold"
+                mt={{ base: 4, md: 0 }}
+                ml={8}
+                display="block"
+                fontSize="xl"
+                fontFamily="diodrum-bold !important"
+            >
+              <Link onClick={handleToggle} to="/cultureStudies">
+                دراسات ثقافية
+              </Link>
+            </Text>
+            <Text
+                fontWeight="bold"
+                mt={{ base: 4, md: 0 }}
+                ml={8}
+                display="block"
+                fontSize="xl"
+                fontFamily="diodrum-bold !important"
+            >
+              <Link onClick={handleToggle} to="/books?category=دراسات المركز">
+                كتب المركز
+              </Link>
+            </Text>
+            <Text
+                fontWeight="bold"
+                mt={{ base: 4, md: 0 }}
+                ml={8}
+                display="block"
+                fontSize="xl"
+                fontFamily="diodrum-bold !important"
+            >
+              <Link onClick={handleToggle} to="/events">
+                نشاطات
+              </Link>
+            </Text>
+            <Text
+                fontWeight="bold"
+                mt={{ base: 4, md: 0 }}
+                ml={8}
+                display="block"
+                fontSize="xl"
+                fontFamily="diodrum-bold !important"
+            >
+              <Link onClick={handleToggle} to="/standards">
+                المنهجية
+              </Link>
+            </Text>
+            {/* <Text             fontWeight="bold"
  mt={{ base: 4, md: 0 }} ml={8} display="block" fontSize="xl">
           <Link onClick={handleToggle} to="#">
             تسجيل
@@ -149,18 +182,148 @@ export default function Navbar(props) {
             </Button>
           </Link>
         </Text> */}
-        <Box
-          cursor="pointer"
-          mr={['0', '0', '6', '6']}
-          ml={['0', '0', '6', '6']}
-          mt={['6', '6', '0', '0']}
-          fontSize="24px"
-          onClick={toggleColorMode}
+            <Box
+                cursor="pointer"
+                mr={['0', '0', '6', '6']}
+                ml={['0', '0', '6', '6']}
+                mt={['6', '6', '0', '0']}
+                fontSize="24px"
+                onClick={toggleColorMode}
+            >
+              {' '}
+              {colorMode === 'light' ? <FaMoon></FaMoon> : <FaSun></FaSun>}
+            </Box>
+          </Box>
+        </Flex>
+
+        <Drawer
+            // style={{dir:"rtl"}}
+
+            isOpen={isOpen}
+            placement="right"
+            onClose={onClose}
+            finalFocusRef={btnRef}
         >
-          {' '}
-          {colorMode === 'light' ? <FaMoon></FaMoon> : <FaSun></FaSun>}
-        </Box>
-      </Box>
-    </Flex>
+          <DrawerOverlay />
+          <DrawerContent
+              color={color[colorMode]}
+              bg={bg[colorMode]}>
+            {/*<DrawerCloseButton/>*/}
+            <DrawerHeader>
+              <Flex
+                  wrap={'nowrap'}
+                  style={{justifyContent:"space-between"}}
+              >
+                <Link onClick={onClose} to="/">
+                  <Heading
+                      cursor="pointer"
+                      fontWeight="bold"
+                      as="h1"
+                      fontSize="36px"
+                      letterSpacing={'-.1rem'}
+                  >
+                    <Image
+                        loading="lazy"
+                        w="200px"
+                        className="main-logo"
+                        src={logo}
+                    ></Image>
+                  </Heading>
+                </Link>
+                <Box
+                    cursor="pointer"
+                    fontSize="24px"
+                    onClick={toggleColorMode}
+                >
+                  {' '}
+                  {colorMode === 'light' ? <FaMoon></FaMoon> : <FaSun></FaSun>}
+                </Box>
+              </Flex>
+            </DrawerHeader>
+
+            <DrawerBody>
+              <Text
+                  fontFamily="diodrum-bold !important"
+                  mt={{ base: 4, md: 0 }}
+                  ml={8}
+                  display="block"
+                  fontWeight="bold"
+                  fontSize="lg"
+              >
+                <Link onClick={onClose} to="/about">عن المركز</Link>
+              </Text>
+              <Text
+                  fontFamily="diodrum-bold !important"
+                  mt={{ base: 4, md: 0 }}
+                  ml={8}
+                  display="block"
+                  fontWeight="bold"
+                  fontSize="lg"
+              >
+                <Link onClick={onClose} to="/enthropology">المختار الإنثربولوجي</Link>
+              </Text>
+
+
+              <Text
+                  fontFamily="diodrum-bold !important"
+                  mt={{ base: 4, md: 0 }}
+                  ml={8}
+                  display="block"
+                  fontWeight="bold"
+                  fontSize="lg"
+              >
+                <Link onClick={onClose} to="/cultureStudies">
+                  دراسات ثقافية
+                </Link>
+              </Text>
+
+              <Text
+                  fontFamily="diodrum-bold !important"
+                  mt={{ base: 4, md: 0 }}
+                  ml={8}
+                  display="block"
+                  fontWeight="bold"
+                  fontSize="lg"
+              >
+                <Link onClick={onClose} to="/books?category=دراسات المركز">
+                  كتب المركز
+                </Link>
+              </Text>
+
+
+              <Text
+                  fontFamily="diodrum-bold !important"
+                  mt={{ base: 4, md: 0 }}
+                  ml={8}
+                  display="block"
+                  fontWeight="bold"
+                  fontSize="lg"
+              >
+                <Link onClick={onClose} to="/events">
+                  نشاطات
+                </Link>
+              </Text>
+
+              <Text
+                  fontFamily="diodrum-bold !important"
+                  mt={{ base: 4, md: 0 }}
+                  ml={8}
+                  display="block"
+                  fontWeight="bold"
+                  fontSize="lg"
+              >
+                <Link onClick={onClose} to="/about">
+                  المنهجية
+                </Link>
+              </Text>
+
+            </DrawerBody>
+            <DrawerFooter>
+
+            </DrawerFooter>
+          </DrawerContent>
+        </Drawer>
+      </>
+
   );
 }
