aeb0b63e24ac9276f571dfec7ad857d43a6fe818
mohamed <ma.belhadj@esi-sba.dz>
2024-08-26 11:17:49 +0100

    fix bugs , mix and shuffle the articles+books

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const fs = require("fs");
 const jsdom  = require("jsdom");
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 const headers = {
--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,6 @@ import {
   useMediaQuery,
 } from '@chakra-ui/core';
 import { Link } from 'react-router-dom';
-// import fx from 'money';
 import { connect } from 'react-redux';
 import { getHome } from '../redux/actions/homeActions';
 
@@ -23,15 +22,10 @@ function Home({ getHome }) {
 
   const [data, setData] = useState(null);
   const [loaded, setLoaded] = useState(false);
+  const [shuffledItems, setShuffledItems] = useState([]);
 
   const bg = { light: 'white', dark: '#151a23' };
 
-  const breakpointColumnsObj = {
-    default: 3,
-    1300: 3,
-    1000: 2,
-    600: 1,
-  };
   const breakpointColumns = {
     default: 4,
     1300: 3,
@@ -45,14 +39,104 @@ function Home({ getHome }) {
       const res = await getHome();
       if (res) {
         setData(res.data);
+        
+        // Combine and shuffle articles and books
+        const combinedItems = [
+          ...(res.data.articles || []).map(item => ({ ...item, type: 'article' })),
+          ...(res.data.books || []).map(item => ({ ...item, type: 'book' }))
+        ];
+        setShuffledItems(shuffleArray(combinedItems));
       }
-      console.log(res.data)
     }
     getData();
   }, []);
 
+  const shuffleArray = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  };
+
   const imageLoaded = () => {
     setLoaded(true);
+  };
+
+  const renderItem = (item) => {
+    if (item.type === 'article') {
+      const articleBody = item.body?.split('\n');
+      const body =
+        articleBody && articleBody.length > 1
+          ? articleBody[0] + '' + articleBody[1]
+          : articleBody?.[0] || '';
+
+      return (
+        <Box mb="1.5em" mx={isSmallerThan500 ? '0' : '1em'} key={item.id}>
+          <Link to={`/singlePost?id=${item.id}`}>
+            <Box
+              bg={bg[colorMode]}
+              w="100%"
+              shadow="lg"
+              pb="4"
+              mt="8"
+              cursor="pointer"
+            >
+              <Skeleton w="100%" isLoaded={loaded}>
+                <Image
+                  loading="lazy"
+                  w="100%"
+                  onLoad={imageLoaded}
+                  src={`${process.env.REACT_APP_STORAGE}/${item.image}`}
+                />
+              </Skeleton>
+              <Box p="1em">
+                <Text fontFamily="diodrum-med !important" mb="4">
+                  {item.author || item.author_optional}
+                </Text>
+                <Heading fontFamily="diodrum-bold !important" mb="4">
+                  {item.title}
+                </Heading>
+                <Box
+                  fontSize="xl"
+                  className="content books__content event-body"
+                >
+                  <Box dangerouslySetInnerHTML={{ __html: body }} />
+                </Box>
+              </Box>
+            </Box>
+          </Link>
+        </Box>
+      );
+    } else if (item.type === 'book') {
+      return (
+        <Box mb="1.5em" mx="1em" key={item.id}>
+          <Link to={`/book/${item.id}`}>
+            <Box my="8" pb="4" shadow="lg" bg={bg[colorMode]}>
+              <Image
+                loading="lazy"
+                mt="2"
+                w="100%"
+                src={`${process.env.REACT_APP_STORAGE}/${item.cover}`}
+              />
+              <Box p="1em">
+                <Text fontFamily="diodrum-med !important" mb="4">
+                  {item?.author?.[0]?.name || item?.author?.[0]?.author_optional}
+                </Text>
+                <Heading fontFamily="diodrum-bold !important" mb="4">
+                  {item.title}
+                </Heading>
+                <Box
+                  fontSize="xl"
+                  className="content books__content"
+                  dangerouslySetInnerHTML={{ __html: item.overview }}
+                />
+              </Box>
+            </Box>
+          </Link>
+        </Box>
+      );
+    }
   };
 
   return (
@@ -73,102 +157,20 @@ function Home({ getHome }) {
         />
       </Box>
       <Box>
-        <Box mb="2em" d="flex" justifyContent="center">
-          <Masonry
-            breakpointCols={breakpointColumnsObj}
-            className="my-masonry-grid"
-            columnClassName="my-masonry-grid_column"
-          >
-           {data &&
-  data.articles &&
-  data.articles.map(article => {
-    const articleBody = article.body?.split('\n');
-    const body =
-      articleBody && articleBody.length > 1
-        ? articleBody[0] + '' + articleBody[1]
-        : articleBody?.[0] || '';
-
-    return (
-      <Box mb="1.5em" mx={isSmallerThan500 ? '0' : '1em'} key={article.id}>
-        <Link to={`/singlePost?id=${article.id}`}>
-          <Box
-            bg={bg[colorMode]}
-            w="100%"
-            shadow="lg"
-            pb="4"
-            mt="8"
-            cursor="pointer"
-          >
-            <Skeleton w="100%" isLoaded={loaded}>
-              <Image
-                loading="lazy"
-                w="100%"
-                onLoad={imageLoaded}
-                src={`${process.env.REACT_APP_STORAGE}/${article.image}`}
-              />
-            </Skeleton>
-            <Box p="1em">
-              <Text fontFamily="diodrum-med !important" mb="4">
-                {article.author || article.author_optional}
-              </Text>
-              <Heading fontFamily="diodrum-bold !important" mb="4">
-                {article.title}
-              </Heading>
-              <Box
-                fontSize="xl"
-                className="content books__content event-body"
-              >
-                <Box dangerouslySetInnerHTML={{ __html: body }} />
-              </Box>
-            </Box>
-          </Box>
-        </Link>
-      </Box>
-    );
-  })}
-          </Masonry>
-        </Box>
         <Box d="flex" justifyContent="center">
           <Masonry
             breakpointCols={breakpointColumns}
             className="my-masonry-grid"
             columnClassName="my-masonry-grid_column"
           >
-           {data &&
-  data.books &&
-  data.books.map(book => (
-    <Box mb="1.5em" mx="1em" key={book.id}>
-      <Link to={`/book/${book.id}`}>
-        <Box my="8" pb="4" shadow="lg" bg={bg[colorMode]}>
-          <Image
-            loading="lazy"
-            mt="2"
-            w="100%"
-            src={`${process.env.REACT_APP_STORAGE}/${book.cover}`}
-          />
-          <Box p="1em">
-            <Text fontFamily="diodrum-med !important" mb="4">
-              {book?.author?.[0]?.name || book?.author?.[0]?.author_optional}
-            </Text>
-            <Heading fontFamily="diodrum-bold !important" mb="4">
-              {book.title}
-            </Heading>
-            <Box
-              fontSize="xl"
-              className="content books__content"
-              dangerouslySetInnerHTML={{ __html: book.overview }}
-            />
-          </Box>
-        </Box>
-      </Link>
-    </Box>
-  ))}
+            {shuffledItems.map(item => renderItem(item))}
           </Masonry>
         </Box>
       </Box>
     </Box>
   );
 }
+
 const mapDispatchToProps = dispatch => {
   return { getHome: () => dispatch(getHome()) };
 };
